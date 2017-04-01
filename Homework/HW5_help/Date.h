509786bcb4ca75849b7ad0bd01ba98a22c45f9ff
joe.dumoulin <joe.dumoulin@gmail.com>
2017-03-31 17:57:50 -0700

    help for HW5 problem 12.25

---
+++ Homework/HW5_help/Date.h
@@ -0,0 +1,27 @@
+// Date.h - define a simple class to hold dates
+//
+#ifndef DATE_H_
+#define DATE_H_
+
+
+
+class Date
+{
+    private:
+        int _year;
+        int _month;
+        int _day;
+
+    public:
+        Date();
+        Date(int time_since_epoch);
+        Date(int year, int month, int day);
+
+        void setDate(int time_since_epoch);
+        int getYear() const;
+        int getMonth() const;
+        int getDay() const;
+
+};
+
+#endif // DATE_H_
